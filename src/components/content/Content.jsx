f30ae73d57c186d88ef37a0b9edaa9aa61b30d08
gretaTomasoni <greta51198@gmail.com>
2023-03-07 18:38:28 +0100

    Home desktop

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,9 +1,11 @@
+import HeaderDesktop from "../headerDesktop";
 import TweetList from "../tweetList";
 import "./index.css";
 
 const Content = () => {
   return (
     <div className="Content">
+      <HeaderDesktop />
       <TweetList />
     </div>
   );
